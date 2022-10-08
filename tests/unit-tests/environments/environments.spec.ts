93f5dbf0cbea27fc5df390b59832d2fbf0fb2ed5
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-08 12:34:34 +0530

    Initial Cmmit

---
+++ tests/unit-tests/environments/environments.spec.ts
@@ -0,0 +1,35 @@
+import 'jest';
+import Environment from '../../../src/environments/environment';
+import { Environments } from '../../../src/environments/environment.constant';
+
+describe('Environment', () => {
+  let instance: Environment;
+
+  beforeEach(() => {
+    instance = new Environment('local');
+  });
+
+  it('should get the current environment', async () => {
+    expect(instance).toBeInstanceOf(Environment);
+    const environment = instance.getCurrentEnvironment();
+    expect(environment).toBeDefined();
+    expect(environment).toBe(Environments.TEST);
+  });
+
+  it('should check if environement is production or not', async () => {
+    const result = instance.isProductionEnvironment();
+    expect(result).toBe(false);
+  });
+
+  it('should check if environement is production or not', async () => {
+    const result = instance.isProductionEnvironment();
+    expect(result).toBe(false);
+  });
+
+  it('should set the current environment', async () => {
+    instance.setEnvironment(Environments.TEST);
+    const environment = instance.getCurrentEnvironment();
+    expect(environment).toBeDefined();
+    expect(environment).toBe(Environments.TEST);
+  });
+});
