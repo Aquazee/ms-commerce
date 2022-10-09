c00eefd4edad0d9ebec68bb93e25929dfa755706
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-09 14:43:01 +0530

    Added Exception handling and mongodb database connection

---
+++ src/lib/service-error-handler.ts
@@ -0,0 +1,26 @@
+import logger from '../lib/logger';
+
+const serviceErrorHandler: any = {};
+
+function logError(errHead: string, err: Error) {
+  logger.info(errHead);
+  logger.error(err.name);
+  logger.error(err.message);
+  logger.error(err.stack);
+}
+
+serviceErrorHandler.sigError = (signal: any) => {
+  logger.info(`Received ${signal}`);
+};
+
+serviceErrorHandler.handleExRe = (reason: Error) => {
+  logError('Unhandled reason:', reason);
+  logger.error(reason.stack);
+  // application specific logging, throwing an error, or other logic here
+};
+
+serviceErrorHandler.beforeExitHandler = () => {
+  logger.info('Process beforeExit, Add Cleanup functions ');
+};
+
+export default serviceErrorHandler;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import * as http from 'http';
 import { AddressInfo } from 'net';
+import mongoose from 'mongoose';
+
 import App from './App';
 import Environment from './environments/environment';
 import { setGlobalEnvironment } from './global';
 import logger from './lib/logger';
+import serviceErrorHandler from './lib/service-error-handler';
 
 const env: Environment = new Environment();
 setGlobalEnvironment(env);
@@ -26,22 +29,20 @@ function serverListening(): void {
 app
   .init()
   .then(() => {
-    app.express.set('port', env.port);
-
-    server = app.httpServer; // http.createServer(App);
-    server.on('error', serverError);
-    server.on('listening', serverListening);
-    server.listen(env.port);
+    mongoose.connect(process.env.DB_URL as string).then((res) => {
+      logger.info('Db Connected');
+      app.express.set('port', env.port);
+      server = app.httpServer; // http.createServer(App);
+      server.on('error', serverError);
+      server.on('listening', serverListening);
+      server.listen(env.port);
+    });
   })
-  .catch((err: Error) => {
-    logger.info('app.init error');
-    logger.error(err.name);
-    logger.error(err.message);
-    logger.error(err.stack);
-  });
+  .catch(serviceErrorHandler.handleExRe);
 
-process.on('unhandledRejection', (reason: Error) => {
-  logger.error('Unhandled Promise Rejection: reason:', reason.message);
-  logger.error(reason.stack);
-  // application specific logging, throwing an error, or other logic here
-});
+process.on('unhandledRejection', serviceErrorHandler.handleExRe);
+process.on('uncaughtException', serviceErrorHandler.handleExRe);
+process.on('beforeExit', serviceErrorHandler.beforeExitHandler);
+process.on('exit', serviceErrorHandler.sigError);
+process.on('SIGINT', serviceErrorHandler.sigError);
+process.on('SIGTERM', serviceErrorHandler.sigError);
