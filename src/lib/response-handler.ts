034fe3e9fa8689a280845e81c154b77809eca624
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-09 14:55:44 +0530

    Removed unwanted file

--- src/lib/response-handler.ts
+++ src/lib/response-handler.ts
@@ -6,24 +6,14 @@ import logger from './logger';
 function send(res: Response): void {
   let obj = {};
   obj = res.locals.data;
-  if (environment.isProductionEnvironment()) {
-    logger.info(JSON.stringify(obj, null, 2));
-  }
-  if (environment.applyEncryption) {
-    obj = Crypto.encrypt(JSON.stringify(obj), environment.secretKey);
-  }
+  logger.info(JSON.stringify(obj, null, 2));
   res.status(StatusCodes.OK).send(obj);
 }
 
 function json(res: Response): void {
   let obj = {};
   obj = res.locals.data;
-  if (environment.isProductionEnvironment()) {
-    logger.info(JSON.stringify(obj, null, 2));
-  }
-  if (environment.applyEncryption) {
-    obj = Crypto.encrypt(JSON.stringify(obj), environment.secretKey);
-  }
+  logger.info(JSON.stringify(obj, null, 2));
   res.status(StatusCodes.OK).json(obj);
 }
 
