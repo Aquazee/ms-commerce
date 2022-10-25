1bbb777deedfdfec288efb78f0a42de133824979
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-25 13:13:18 +0530

    Added Error Objects

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -12,6 +12,30 @@ export const UserError = {
     message: 'Email already taken',
     status: httpStatus.CONFLICT,
   },
+
+  UserBlocked: {
+    error: 'UserBlocked',
+    message: 'User is Blocked',
+    status: httpStatus.UNAUTHORIZED,
+  },
+
+  UserInactive: {
+    error: 'UserInactive',
+    message: 'User is Inactive, Please reactivate your account.',
+    status: httpStatus.UNAUTHORIZED,
+  },
+
+  InvalidId: {
+    error: 'InvalidId',
+    message: 'Invalid Id.',
+    status: httpStatus.BAD_REQUEST,
+  },
+
+  InvalidToken: {
+    error: 'InvalidToken',
+    message: 'Invalid Token.',
+    status: httpStatus.BAD_REQUEST,
+  },
 };
 
 export const ProductError = {
@@ -19,5 +43,20 @@ export const ProductError = {
     error: 'ProductNotFound',
     message: 'Product not found',
     status: httpStatus.NOT_FOUND,
+  },
+  ProductInactive: {
+    error: 'ProductInactive',
+    message: 'Product is Inactive.',
+    status: httpStatus.BAD_REQUEST,
+  },
+  ProductCustomLabelTaken: {
+    error: 'Product CustomLabelTaken',
+    message: 'Product custom label taken.',
+    status: httpStatus.BAD_REQUEST,
+  },
+  ProductTitleTaken: {
+    error: 'ProductTitleTaken',
+    message: 'Product title taken.',
+    status: httpStatus.BAD_REQUEST,
   },
 };
