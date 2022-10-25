4fea038148f9e15d40a53cc20be00b58092f8c8d
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-25 13:12:58 +0530

    Added Helper Functions

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,4 +1,37 @@
+import { Express, Request } from 'express';
 import mongoose from 'mongoose';
+import requestIp from 'request-ip';
+import { UserError } from './errors';
 
 export const convertToObjectId = (id: string) =>
   new mongoose.Types.ObjectId(id);
+
+/**
+ * Check if Object Id sent is valid
+ * @param {String} id
+ */
+export const isValidObjectID = async (id: string) => {
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    return id;
+  }
+  throw UserError.InvalidId;
+};
+
+/**
+ * Gets IP from user
+ * @param {*} req - request object
+ */
+export const getIP = (req: Request) => requestIp.getClientIp(req);
+
+/**
+ * Gets browser info from user
+ * @param {*} req - request object
+ */
+export const getBrowserInfo = (req: Request) => req.headers['user-agent'];
+
+/**
+ * Gets country from user using CloudFlare header 'cf-ipcountry'
+ * @param {*} req - request object
+ */
+export const getCountry = (req: Request) =>
+  req.headers['cf-ipcountry'] ? req.headers['cf-ipcountry'] : 'XX';
